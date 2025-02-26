5ee46a59e10590cfe9710e52d1a8c469e1b3a1f3
Eros Mendoza <mendozaeros80@gmail.com>
2025-02-25 20:08:44 -0800

    Ready to grade!

--- tests/SumNumbersTest.java
+++ tests/SumNumbersTest.java
@@ -1,2 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class SumNumbersTest {
+    @org.junit.jupiter.api.Test
+    void ifEvenTest1() {
+        SumNumbers sumNumbers = new SumNumbers();
+        int result = sumNumbers.sumIsEven(1,1);
+        assertEquals(1, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void ifEvenTest2() {
+        SumNumbers sumNumbers = new SumNumbers();
+        int result = sumNumbers.sumIsEven(1,2);
+        assertEquals(0, result);
+    }
 }
--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TipCalculatorTest {
+    @org.junit.jupiter.api.Test
+    void tipTest1() {
+        TipCalculator tipCalculator = new TipCalculator();
+        double result = tipCalculator.calculateTip(100.00,20);
+        assertEquals(20.00, result);
+    }
+
+    @org.junit.jupiter.api.Test
+    void tipTest2() {
+        TipCalculator tipCalculator = new TipCalculator();
+        double result = tipCalculator.calculateTip(25.00,25);
+        assertEquals(6.25, result);
+    }
 }
